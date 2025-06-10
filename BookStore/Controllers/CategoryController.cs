a39ce72a0b6642e40d0450e20abbad2e8ad36bd9
veronosmani <veron.osmani@gmail.com>
2025-06-10 10:35:45 +0200

    3

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -33,6 +33,7 @@ namespace BookStore.Controllers
             if (ModelState.IsValid) {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
@@ -40,32 +41,62 @@ namespace BookStore.Controllers
 
         public IActionResult Edit(int? CID)
         {
-            if (CID==null || CID == 0)
+            if (CID == null || CID == 0)
             {
                 return NotFound();
             }
-            return View();
+            Category? categoryFromDb = _db.Categories.Find(CID);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.CID==CID);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.CID==CID).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
         }
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("Name", "The Display Order cannot match the Category Name");
-            }
-
-            if (obj.Name != null && obj.Name == "Test")
-            {
-                ModelState.AddModelError("DisplayOrder", "Test is an invalid name.");
-            }
-
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
+        }
+        public IActionResult Delete(int? CID)
+        {
+            if (CID == null || CID == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(CID);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.CID==CID);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.CID==CID).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? CID)
+        {
+            Category? obj = _db.Categories.Find(CID);
+                
+            if (obj == null)
+            {
+                return NotFound();
+            }   
+            
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index", "Category");
         }
     }
 }
---
+++ Bookstore_Razor/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore_Razor.Models
+{
+    public class Category {
+        [Key]
+        public int CID { get; set; }
+
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30, ErrorMessage = "Category Name can't be longer than 30 characters.")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Required]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100.")]
+        public int? DisplayOrder { get; set; }
+
+    }
+}
